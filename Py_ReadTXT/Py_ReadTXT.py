d9310f9a24f43ee5d7d9295279393864205551ec
xu <779372438@qq.com>
2023-08-19 16:18:49 +0800

    complete proj

--- Py_ReadTXT/Py_ReadTXT.py
+++ Py_ReadTXT/Py_ReadTXT.py
@@ -2,9 +2,6 @@ import msvcrt
 import json
 TXTPath = './KSLSH1.txt'
 NotePath ='./BookNote.json'
-#打开txt文件
-ftext = open(TXTPath, encoding='utf8')
-
 #打开并且读取json数据
 def read_json(path):
     with open(path,'r') as fnote:
@@ -16,23 +13,46 @@ def write_json(path,params):
     with open(path, 'w') as f:
         json.dump(params,f,indent=2)
 
-
-params = read_json(NotePath)
-#for i,line in enumerate(ftext):
+#打开txt文件
+ftext = open(TXTPath, encoding='utf8')
 lines = ftext.readlines()
-#读取书签行 
-i=params["Note_Line"]
+#读取json书签
+params = read_json(NotePath)
+#判断是否同一本书，同一本下才能读取书签
+bookname = params["Book_Name"]
+if(bookname == TXTPath[2:]):
+    i=params["Note_Line"]
+else:
+    params["Book_Name"] = TXTPath[2:]
+    write_json(NotePath,params)
+    i=1
+
 print(lines[i])
 while(1):
     if msvcrt.kbhit():
+        #判断ESC
         if msvcrt.getch() != b'\x1b':
+            #skip all empty line
+            while(i<len(lines)-1):
+                if(lines[i]!='\n'):
+                    break
+                else:
+                    i += 1 
+
             if(lines[i]!='\n'):
                 print(lines[i])
                 params["Note_Line"] = i
-            i = i+1
+                i += 1
+            else:
+                print("END PAGE")
+                params["Note_Line"] = 1
+                write_json(NotePath,params)
+                print("note start line OK")
+                break   
         else:
+            #note line to json 
             write_json(NotePath,params)
-            print("note OK")
+            print("note line "+str(i-1)+" OK ")
             break;
 #json文件保存数据    
     
